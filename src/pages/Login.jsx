25889148312aeffcd7dcb1e5e80f8d90e15fbe32
Venkat <venka7m@gmail.com>
2023-06-05 03:42:29 +0530

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import '../css/Login.css'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-const BASE_URL = 'https://conduit-706z.onrender.com/'
+const BASE_URL = 'https://conduit-706z.onrender.com'
 axios.defaults.baseURL = 'https://conduit-backend-7t9u.onrender.com'
 axios.defaults.withCredentials = true
 
